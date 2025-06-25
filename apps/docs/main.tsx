5d1ee2362c10defb74e19f3d018d1db44db88387
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-06-25 10:30:22 +0100

    Update main.tsx

--- apps/docs/main.tsx
+++ apps/docs/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Splashscreen } from "@acme/ui";
 
 const root = document.getElementById("root") as HTMLElement;
+// Trigger redeploy
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
