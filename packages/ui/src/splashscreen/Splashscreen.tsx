777f3adc065b82f6be90cae64eb7144cea655363
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-06-25 08:12:51 +0100

    Update Splashscreen.tsx

--- packages/ui/src/splashscreen/Splashscreen.tsx
+++ packages/ui/src/splashscreen/Splashscreen.tsx
@@ -1,1 +1,35 @@
+import React from "react";
+import x97604931 from "./9760493-1.png";
+import x97604933 from "./9760493-3.png";
+import { HomeIndicator } from "./HomeIndicator";
+import "./style.css";
+
+export const Splashscreen = (): JSX.Element => {
+  return (
+    <div className="splashscreen">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <img className="element" alt="Element Layer 1" src={x97604933} />
+          <img className="img" alt="Element Layer 2" src={x97604931} />
+
+          <div className="group">
+            <div className="logo">
+              <div className="group-2" />
+            </div>
+            <div className="text-wrapper">OptiGrade</div>
+          </div>
+
+          <HomeIndicator
+            className="home-indicator-instance"
+            device="i-phone"
+            homeIndicatorClassName="design-component-instance-node"
+            orientation="portrait"
+          />
+
+          <p className="done">Tap the screen to launch</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
