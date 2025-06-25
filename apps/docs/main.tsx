afe2909c882184688443e97fdac4268a864597dd
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-06-25 09:40:39 +0100

    Create main.tsx

---
+++ apps/docs/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Splashscreen } from "@acme/ui";
+
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Splashscreen />
+    </BrowserRouter>
+  </React.StrictMode>
+);
